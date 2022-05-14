3d0fe2c6521b7ab6371b9ac457e61b00f00e7cfc
Lekinio <lekishvilitornike@gmail.com>
2022-05-15 00:44:30 +0400

    --Initial commit

--- src/statistics/test/statistics.controller.spec.ts
+++ src/statistics/test/statistics.controller.spec.ts
@@ -1,23 +1,89 @@
+import {
+  getRepositoryToken,
+  getConnectionToken,
+  TypeOrmModule
+} from '@nestjs/typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
-import { StatisticsController } from '../statistics.controller';
+import { Provider } from '@nestjs/common';
+import { DataSource, DataSourceOptions, Repository } from 'typeorm';
 import { StatisticsService } from '../statistics.service';
+import { StatisticsEntity, CountriesEntity } from '../../database';
 
 describe('StatisticsController', () => {
-  let controller: StatisticsController;
+  let statisticsService: StatisticsService;
+  let statisticsRepository: Repository<StatisticsEntity>;
+  let countriesRepository: Repository<CountriesEntity>;
+  let dbConnection;
 
-  beforeEach(async () => {
+  const statistic = {
+    confirmed: 100,
+    recovered: 80,
+    deaths: 20,
+    createdDate: new Date(),
+    UpdatedDate: new Date()
+  };
+  const prepareDatabase = async (): Promise<void> => {
+    const country = await countriesRepository.save(
+      countriesRepository.create({
+        en: 'Georgia',
+        ka: 'საქართველო',
+        code: 'Test',
+        createdDate: new Date(),
+        UpdatedDate: new Date()
+      } as CountriesEntity)
+    );
+
+    await statisticsRepository.save(
+      statisticsRepository.create({
+        ...statistic,
+        country: country
+      } as StatisticsEntity)
+    );
+  };
+
+  beforeAll(async () => {
+    const connectionOptionsForTesting: DataSourceOptions = {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'rootroot',
+      database: 'covid_statistics_test',
+      synchronize: true
+    };
+
+    const databaseProviders: Provider[] = [
+      {
+        provide: DataSource,
+        useFactory: async () => {
+          const dataSource = new DataSource({
+            ...connectionOptionsForTesting,
+            entities: [CountriesEntity, StatisticsEntity]
+          });
+          return await dataSource.initialize();
+        }
+      }
+    ];
+
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [StatisticsController],
-      providers: [StatisticsService],
+      imports: [TypeOrmModule.forRoot()],
+      providers: [StatisticsService, ...databaseProviders]
     }).compile();
 
-    controller = module.get<StatisticsController>(StatisticsController);
+    statisticsService = module.get<StatisticsService>(StatisticsService);
+    statisticsRepository = module.get(getRepositoryToken(StatisticsEntity));
+    dbConnection = module.get(getConnectionToken());
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  beforeEach(async () => {
+    await dbConnection.query(`TRUNCATE statistics CASCADE`);
+    await prepareDatabase();
   });
-  it('should return countries statistics', () => {
 
-  })
+  it('should get all statistics', async () => {
+    const result = statisticsService.getAll();
+    console.log(result);
+
+    expect(result).toEqual([statistic]); // check this
+  });
 });
